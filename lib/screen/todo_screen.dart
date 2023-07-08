607f73667a8ee206ae7d3a810c6ed77e4915bd72
TNQT14 <2002quangthai@gmail.com>
2023-07-08 19:27:50 +0700

    Toto Screen
    UI

--- lib/screen/todo_screen.dart
+++ lib/screen/todo_screen.dart
@@ -8,9 +8,14 @@ class TodoScreen extends StatefulWidget {
 }
 
 class _TodoScreenState extends State<TodoScreen> {
+  var _selectValue;
+  var _categories;
+
   @override
   Widget build(BuildContext context) {
-    var todoTitleController;
+    var todoTitleController =TextEditingController();
+    var todoDescriptionController = TextEditingController();
+    var todoDateController = TextEditingController();
 
     return Scaffold(
       appBar: AppBar(
@@ -22,9 +27,48 @@ class _TodoScreenState extends State<TodoScreen> {
             controller: todoTitleController,
             decoration: InputDecoration(
               labelText: 'Title',
-              hintText: 'Write todo title',
+              hintText: 'Write Todo Title',
             ),
-          )
+          ),
+          TextField(
+            controller: todoDescriptionController,
+            decoration: InputDecoration(
+              labelText: 'Description',
+              hintText: 'Write Todo Description',
+            ),
+          ),
+          TextField(
+            controller: todoDateController,
+            decoration: InputDecoration(
+              labelText: 'Date',
+              hintText: 'Pick a Date',
+              prefixIcon: InkWell(
+                onTap: (){},
+                child: Icon(Icons.calendar_today),
+              )
+            ),
+          ),
+          DropdownButtonFormField(
+            value: _selectValue,
+            items: _categories,
+            hint: Text('Category'),
+            onChanged: (value){
+              setState(() {
+                _selectValue = value;
+              });
+            },
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          ElevatedButton(
+              onPressed: (){},
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.blue), // Màu nền khi nút không được nhấn
+                foregroundColor: MaterialStateProperty.all<Color>(Colors.white), // Màu chữ khi nút không được nhấn
+                overlayColor: MaterialStateProperty.all<Color>(Colors.blueAccent), // Màu nền khi nút được nhấn
+              ),
+              child: Text('Save', style: TextStyle(color: Colors.white)))
         ],
       ),
     );
